3de609b7b04e22799582e60c314c2f2f63f1ce4f
Piaaa15 <fadiyahsarii@gmail.com>
2024-04-29 20:38:29 +0800

    Buat Nama

--- app/src/main/java/com/example/project_mently/BuatNama.java
+++ app/src/main/java/com/example/project_mently/BuatNama.java
@@ -8,13 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class BuatNamaSandi extends AppCompatActivity {
+public class BuatNama extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_buat_nama_sandi);
+        setContentView(R.layout.activity_buat_nama);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
